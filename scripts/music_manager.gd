9f482b53130b8b58ba18569204331d079d313cb8
Julian L <julian.lawrance@gmail.com>
2024-08-04 13:42:17 +0100

    Play different audio clip after a high guess

--- scripts/music_manager.gd
+++ scripts/music_manager.gd
@@ -3,9 +3,22 @@ extends Node
 @export
 var main_audio: AudioStream
 
+@export
+var high_guess_audio: AudioStream
+
 @onready
 var music_player: AudioStreamPlayer = %Music
 
 func _ready():
     music_player.stream = main_audio
     music_player.play()
+
+func _on_game_manager_guessed_high():
+    music_player.stream = high_guess_audio
+    music_player.play()
+
+    music_player.finished.connect(
+        func():
+            music_player.stream = main_audio
+            music_player.play()
+    )
