2f7a1da4413cd51f9173e7313dd9b8ae5e7f5025
Julian L <julian.lawrance@gmail.com>
2024-08-04 13:52:30 +0100

    Play different audio clip when the game is won

--- scripts/music_manager.gd
+++ scripts/music_manager.gd
@@ -6,6 +6,9 @@ var main_audio: AudioStream
 @export
 var high_guess_audio: AudioStream
 
+@export
+var win_audio: AudioStream
+
 @onready
 var music_player: AudioStreamPlayer = %Music
 
@@ -13,12 +16,19 @@ func _ready():
     music_player.stream = main_audio
     music_player.play()
 
+func set_main():
+    music_player.stream = main_audio
+    music_player.play()
+
 func _on_game_manager_guessed_high():
     music_player.stream = high_guess_audio
     music_player.play()
 
-    music_player.finished.connect(
-        func():
-            music_player.stream = main_audio
-            music_player.play()
-    )
+    music_player.finished.connect(set_main)
+
+func _on_game_manager_game_won():
+    music_player.stream = win_audio
+    music_player.play()
+
+    if music_player.finished.is_connected(set_main):
+        music_player.finished.disconnect(set_main)
